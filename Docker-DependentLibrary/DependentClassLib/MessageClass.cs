70e8864be396908947d855c13357a40ef24a38bd
Imran Arshad <imran30758@hotmail.com>
2019-09-27 22:44:08 +1000

    Dockerize .NET CORE web App with dependent class library

---
+++ Docker-DependentLibrary/DependentClassLib/MessageClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependentClassLib
+{
+    public class MessageClass
+    {
+        public static string Message()
+        {
+            return "I am message from Dependent Lib";
+        }
+    }
+}
---
+++ Docker-DependentLibrary/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DependentClassLib;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; private set; } = MessageClass.Message();
+        public void OnGet()
+        {
+          
+
+        }
+    }
+}
