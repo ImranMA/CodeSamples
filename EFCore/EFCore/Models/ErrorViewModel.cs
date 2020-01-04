c2d7ac342a567d1dd7a5246005074722d8305c36
Imran Arshad <imran30758@hotmail.com>
2020-01-04 17:08:05 +1100

    Sample Code

--- EFCore/EFCore/Models/ErrorViewModel.cs
+++ EFCore/EFCore/Models/ErrorViewModel.cs
@@ -4,7 +4,6 @@ namespace TestEFCore.Models
 {
     public class ErrorViewModel
     {
-
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
